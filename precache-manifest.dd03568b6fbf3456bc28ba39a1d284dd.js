b9d21b9cdbe6b95f30b23857b9bf7048b1ef7f0e
unknown <piyush96arora@gmail.com>
2019-04-14 15:47:00 +0530

    Updates

---
+++ precache-manifest.dd03568b6fbf3456bc28ba39a1d284dd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c0e66557a74dbceddcae",
+    "url": "/indwealth-react/static/js/runtime~main.5287d7c3.js"
+  },
+  {
+    "revision": "6b0eb572c7f16a309f3d",
+    "url": "/indwealth-react/static/js/main.05e75100.chunk.js"
+  },
+  {
+    "revision": "19dd45c7744d98c25a48",
+    "url": "/indwealth-react/static/js/2.90f22249.chunk.js"
+  },
+  {
+    "revision": "6b0eb572c7f16a309f3d",
+    "url": "/indwealth-react/static/css/main.b5159912.chunk.css"
+  },
+  {
+    "revision": "1461f7ea453c01a8aebc3c0a2637920b",
+    "url": "/indwealth-react/index.html"
+  }
+];
